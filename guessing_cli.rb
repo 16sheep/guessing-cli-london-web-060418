0679dd3d31fc9c85352ee804a9dcc618cb885726
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-28 21:24:56 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
-  else  
+  elsif input!= random
     puts "The computer guessed #{random}."
   end
 end
